9f537b26b70dbb14008c5dba48923ae10b278f5a
karaler <karaler@bytelang.com>
2019-11-20 17:40:54 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,6 @@
+### 这是什么？
+这是用于开发kplayer推流（转码）程序的插件开发必要的头文件
+
+### 如何使用
+1. 在插件根目录创建plugin-pkg的submodules，并submoudles update下载开发包到本地
+2. 使用`git clone`将开发的头文件下载的插件根目录中
---
+++ adapter/KPPluginAdapter.h
@@ -0,0 +1,67 @@
+//
+// Created by karaler on 2019-10-29.
+// Author: karaler
+// Description:
+// Others: 
+// Copyright (c) 2019 ${ORGANIZATION_NAME}. All rights reserved.
+//
+
+#ifndef KPLAYER_KPPLUGINADAPTER_H
+#define KPLAYER_KPPLUGINADAPTER_H
+
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <memory>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavutil/opt.h>
+#include <libavfilter/buffersrc.h>
+#include <libavfilter/buffersink.h>
+};
+
+#include "exception/KPFilterException.h"
+#include "util/KPVariables.h"
+#include "util/KPEvent.hpp"
+
+#include "config.h"
+
+class KPPluginAdapter {
+private:
+    AVFilterContext         *filter_context = nullptr;
+    std::string             identify_name;
+    std::thread             *task_thread    = nullptr;
+    std::condition_variable task_condition;
+    std::mutex              task_mutex;
+
+protected:
+    KPFilterType                    filter_type = KP_FILTER_TYPE_NONE;
+    std::string                     filter_name;
+    std::string                     filter_desc;
+    const AVFilter                  *filter     = nullptr;
+    std::shared_ptr<spdlog::logger> logger;
+
+protected:
+    virtual void Task() = 0;
+    void *GetFilterPriv();
+
+public:
+    explicit KPPluginAdapter(std::string identify_name);
+    virtual ~KPPluginAdapter();
+    AVFilterContext *GetFilterContext();
+    const AVFilter *GetFilter();
+    std::string GetIdentifyName();
+    const KPFilterType &GetFilterType();
+    std::string GetFilterName();
+    const std::string &GetFilterDesc();
+    void SetFilterContext(AVFilterContext *filter_ctx);
+    void NotifyTask();
+    void KillTask();
+};
+
+
+#endif //KPLAYER_KPPLUGINADAPTER_H
