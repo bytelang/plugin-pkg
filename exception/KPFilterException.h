73f3f345b2b1ce628c8c999420d9aef94719badb
karaler <karaler@bytelang.com>
2019-11-21 14:50:07 +0800

    添加exception头文件

---
+++ exception/KPFilterException.h
@@ -0,0 +1,27 @@
+//
+// Created by karaler on 2019-09-02.
+// Author: karaler
+// Description:
+// Others: 
+// Copyright (c) 2019 ${ORGANIZATION_NAME}. All rights reserved.
+//
+
+#ifndef KPLAYER_KPFILTEREXCEPTION_H
+#define KPLAYER_KPFILTEREXCEPTION_H
+
+#include <string>
+
+#include "config.h"
+
+class KPFilterException : public std::exception {
+protected:
+    std::string error;
+    int         error_code;
+public:
+    explicit KPFilterException(const std::string &);
+    explicit KPFilterException(const std::string &, const int error_code);
+    std::string GetError();
+};
+
+
+#endif //KPLAYER_KPFILTEREXCEPTION_H
