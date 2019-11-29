486911b8d9f34f1861840efa5ef85e2b208c768e
karaler <karaler@bytelang.com>
2019-11-29 16:02:37 +0800

    feat: 添加InitTask用于初始化数据初始化

--- adapter/KPPluginAdapter.h
+++ adapter/KPPluginAdapter.h
@@ -45,7 +45,14 @@ protected:
     std::shared_ptr<spdlog::logger> logger;
 
 protected:
+    /**
+     * 转码后进行的插件执行任务
+     */
     virtual void Task() = 0;
+
+    /**
+     * 获取avfilter的上下文句柄
+     */
     void *GetFilterPriv();
 
 public:
@@ -58,8 +65,30 @@ public:
     std::string GetFilterName();
     const std::string &GetFilterDesc();
     void SetFilterContext(AVFilterContext *filter_ctx);
+
+    /**
+     * 调度插件task执行
+     */
     void NotifyTask();
+
+    /**
+     * 插件销毁时刻停止当前插件task任务回调函数
+     * 用于插件的reload事件或插件销毁
+     *
+     * example: 在显示当前系统事件插件场景中，在reload或remove操作后将会执行该函数
+     *          函数需要处理当前Task任务的结束，以便异步任务的结束
+     *
+     */
     virtual void KillTask() = 0;
+
+    /**
+     * 插件数据初始化
+     * 用于插件的reload事件或数据初始化
+     *
+     * example: 在显示当前系统时间插件场景中，在reload之后对插件变量的重置操作
+     *          例如在KillTask将定时器停止flag，在此函数中对定时器开启flag进行重置
+     */
+    virtual void InitTask() = 0;
 };
 
 
