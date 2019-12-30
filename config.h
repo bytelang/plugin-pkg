c960098eed1bdd6fe9f5284b22afbe59d87b6156
karaler <karaler@bytelang.com>
2019-12-30 16:46:37 +0800

    feat: 添加helper

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define KPLAYER_PLUGIN_CONFIG_H
 
 
-#define KPLAYER_VERSION_BUILD "v0.2.3"
+#define KPLAYER_VERSION_BUILD "v0.3.0"
 #define KPLAYER_VERSION_PLUGIN "1.0.1"
 
 #define KPLAYER_PLUGIN_FUNC(x) __attribute__ ((visibility ("default"))) x *CreateInstance(PluginParams params)
